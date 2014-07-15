7adf13c402a8835951d539cff46005456b858eb8
Michael Scriven <bigscriv03@gmail.com>
2014-07-15 02:01:52 +0000

    Initial commit of Socialist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialst::Application.config.secret_key_base = '47a705223efd134a2d7f27b95701fd22ed0074f4e37b448b0c5f38f1390181ab3909c5ad9e5e479fb741f6f55f8b0c0aec13b0af65fe75153945cbbc870108ba'
